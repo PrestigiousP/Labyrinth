dae1d79e52cf39443c7ff03f87e68372dd2ab8ce
baillaph <baillaph@uqtr.ca>
2021-03-07 21:24:24 -0500

    mini update

--- WpfApp1/Models/Labyrinth.cs
+++ WpfApp1/Models/Labyrinth.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
 
 
 namespace WpfApp1.ViewModels
